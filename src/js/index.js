3415c56b8c9b6bf5a7a2bf069322e59cf44f1109
ssh891123 <ssh891123@gmail.com>
2023-05-30 22:48:00 +0900

    [feat] All ToDo Done 버튼 기능 추가
    All, ToDo, Done은 라디오 버튼
    
    선택된 버튼에 따라 ToDo List가 보여지는 filter기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -14,11 +14,43 @@ class TodoList {
         this.addBtnEl = this.inputAreaEl.querySelector("#add-btn");
         this.todoContainerEl = document.getElementById('todo-container');
         this.todoListEl = this.todoContainerEl.querySelector("#todo-list");
+        this.radioAreaEl = this.inputContainerEl.querySelector('#radio-area');
+        this.filterRadioBtnEls = this.radioAreaEl.querySelectorAll('input[name="filter"');
     }
 
     addEvent() {
         this.addBtnEl.addEventListener("click", this.onClickAddBtn.bind(this));
         this.todoListEl.addEventListener("click", this.onClickTodoList.bind(this));
+        this.addRadioBtnEvent();
+    }
+
+    addRadioBtnEvent() {
+        for(const filterRadioBtnEl of this.filterRadioBtnEls) {
+            filterRadioBtnEl.addEventListener('click', this.onClickRadioBtn.bind(this));
+        }
+    }
+
+    onClickRadioBtn(event) {
+        const { value } = event.target;
+        console.log(value);
+        this.filterTodo(value);
+    }
+
+    filterTodo(status) {
+        const todoDivEls = this.todoListEl.querySelectorAll('div.todo');
+        for(const todoDivEl of todoDivEls) {
+            switch(status) {
+                case 'ALL':
+                    todoDivEl.style.display = 'flex';
+                    break;
+                case 'DONE':
+                    todoDivEl.style.display = todoDivEl.classList.contains('done') ? 'flex' : 'none';
+                    break;
+                case 'TODO':
+                    todoDivEl.style.display = todoDivEl.classList.contains('done') ? 'none' : 'flex';
+                break;
+            }
+        }
     }
 
     onClickTodoList(event) {
